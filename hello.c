bff54ae44c5b81cba0732b9cdb4ae9be25a9e71b
mainanorbert <mainanorbert@gmail.com>
2023-02-25 20:50:23 +0300

    Reduced prompts

--- hello.c
+++ hello.c
@@ -7,10 +7,9 @@ int main() {
     scanf("%d", &num);  // Read in the user's input and store it in the variable "num"
     printf("Enter a second number");
     scanf("%d", &num2);  // Read in the user's input and store it in the variable "num"
-    printf("Enter a third number:");
-    scanf("%d", &num3);  // Read in the user's input and store it in the variable "num"
+    
 
-    printf("You entered: %d%4d%4d", num, num2, num3);  // Display the user's input
+    printf("You entered: %d%4d", num, num2);  // Display the user's input
 
     return 0;
 }
